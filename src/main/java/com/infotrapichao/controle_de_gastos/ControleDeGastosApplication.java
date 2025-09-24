cc3cd7a16a8eff60d991e6269c7a55d6f9c44ee3
MatheusHard <matheus@novetech.com.br>
2025-09-24 10:35:51 -0300

    fisrt commit

---
+++ src/main/java/com/infotrapichao/controle_de_gastos/ControleDeGastosApplication.java
@@ -0,0 +1,13 @@
+package com.infotrapichao.controle_de_gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeGastosApplication.class, args);
+	}
+
+}
